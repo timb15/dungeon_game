312831f9d2cd78457c1c78dc8b2a27a2986310ab
timb15 <43254717+timb15@users.noreply.github.com>
2019-06-28 11:10:28 -0700

    initial commit

---
+++ dungeon_game.py
@@ -0,0 +1,78 @@
+import os
+import random
+
+#draw grid
+GRID = [(0,0),(1,0), (2,0), (3,0), (4,0),
+       (0,1),(1,1), (2,1), (3,1), (4,1),
+       (0,2),(1,2), (2,2), (3,2), (4,2),
+       (0,3),(1,3), (2,3), (3,3), (4,3),
+       (0,4),(1,4), (2,4), (3,4), (4,4),]
+
+def draw_grid(player):
+    new_grid = GRID[:]
+    for location in new_grid:
+        if player == location:
+            new_grid[new_grid.index(location)] = 	u" \U0001F32E "
+    print(new_grid[0:5])
+    print(new_grid[5:10])
+    print(new_grid[10:15])
+    print(new_grid[15:20])
+    print(new_grid[20:25])
+    
+def generate_locations():
+    return random.sample(GRID, 3)
+    
+def move_player(player, move):
+    x, y = player
+    if move == "LEFT":
+        x -= 1
+    if move == "RIGHT":
+        x += 1
+    if move == "UP":
+        y -= 1
+    if move == "DOWN":
+        y += 1
+    return x,y
+        
+        
+def get_moves(player):
+    x, y = player
+    moves = ["LEFT", "RIGHT", "UP", "DOWN"]
+    if x == 0:
+        moves.remove("LEFT")
+    if x == 4:
+        moves.remove("RIGHT")
+    if y == 0:
+        moves.remove("UP")
+    if y == 4:
+        moves.remove("DOWN")
+    return moves
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+player, monster, door = generate_locations()
+
+while True:
+    print("Welcome to the dungeon!")
+    draw_grid(player)
+    print("You are currently in room {}".format(player))
+    print("You can move {}".format(", ".join(get_moves(player))))
+    print("Enter QUIT to quit")
+    
+    moves = get_moves(player)
+    move = input(">  ").upper()
+    
+    if move == 'QUIT':
+        break
+    if move in moves:
+        player = move_player(player, move)
+    if move not in moves:
+        pass
+    if player == door:
+        print("You found the exit! You Win!!")
+        break
+    if player == monster:
+        print("You found the monster! You Lose!!")
+        break
+    clear_screen()
